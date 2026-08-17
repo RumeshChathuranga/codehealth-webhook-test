401a054914de211b215e9ade316038fb03ab4c83
RumeshChathuranga <rumeshc.23@cse.mrt.ac.lk>
2026-08-17 19:48:23 +0530

    add mod

--- sample.js
+++ sample.js
@@ -2,4 +2,5 @@ function add(a,b){return a+b;}
 function sub(a,b){return a-b;}
 function mul(a,b){return a*b;}
 function div(a,b){return a/b;}
-module.exports={add,sub,mul,div};
+function mod(a,b){return a%b;}
+module.exports={add,sub,mul,div,mod};
