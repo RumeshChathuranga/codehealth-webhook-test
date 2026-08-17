79d66c98a5eb16a61dcf678a6045bc6fb6d783e6
RumeshChathuranga <rumeshc.23@cse.mrt.ac.lk>
2026-08-17 19:44:31 +0530

    add div

--- sample.js
+++ sample.js
@@ -1,13 +1,5 @@
-function add(a, b) {
-  return a + b;
-}
-
-function sub(a, b) {
-  return a - b;
-}
-
-function mul(a, b) {
-  return a * b;
-}
-
-module.exports = { add, sub, mul };
+function add(a,b){return a+b;}
+function sub(a,b){return a-b;}
+function mul(a,b){return a*b;}
+function div(a,b){return a/b;}
+module.exports={add,sub,mul,div};
